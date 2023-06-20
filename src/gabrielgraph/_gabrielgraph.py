fadfc2f48e3c5353bd16e875872ebb97987105e5
Léo Guignard <leo.guignard@univ-amu.fr>
2023-06-20 19:06:24 +0200

    typing better

--- src/gabrielgraph/_gabrielgraph.py
+++ src/gabrielgraph/_gabrielgraph.py
@@ -13,7 +13,7 @@ def build_gabriel_graph(
     pos: np.ndarray,
     data_struct: str = "adj-dict",
     dist: bool = False,
-) -> Union(dict, np.ndarray):
+) -> dict | np.ndarray:
     """
     Build the gabriel graph of a set of nodes with
     associtated positions.
